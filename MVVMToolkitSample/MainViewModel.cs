5640429ccde1244e22fe7304c5e4d907bfe00d85
Marco Seraphin <seraphin@develope-it.de>
2022-03-14 14:46:43 +0100

    First CheckIn

---
+++ MVVMToolkitSample/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MVVMToolkitSample
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        private string? firstName;
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        private string? lastName;
+
+        public string? FullName => $"{FirstName} {LastName}";
+
+        [ICommand]
+        public async Task Save()
+        {
+            await UserDialogs.Instance.AlertAsync("Pressed Save for " + this.FullName);
+        }
+
+        public MainViewModel()
+        {
+            this.firstName = "Marco";
+            this.lastName = "Seraphin";
+        }
+    }
+}
